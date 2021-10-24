122dbb9e0a9133dac1783f1eec4ee98bb98f7287
OnurK.Dev <onurk.dev@outlook.com>
2021-10-24 12:03:29 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace BA_Boost_SayiTahmin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.guess = new System.Windows.Forms.TextBox();
+            this.guessbutton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(262, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı Tahmin Oyunu ";
+            // 
+            // guess
+            // 
+            this.guess.Location = new System.Drawing.Point(327, 115);
+            this.guess.Name = "guess";
+            this.guess.Size = new System.Drawing.Size(117, 26);
+            this.guess.TabIndex = 1;
+            // 
+            // guessbutton
+            // 
+            this.guessbutton.Location = new System.Drawing.Point(327, 161);
+            this.guessbutton.Name = "guessbutton";
+            this.guessbutton.Size = new System.Drawing.Size(117, 44);
+            this.guessbutton.TabIndex = 2;
+            this.guessbutton.Text = "Dene";
+            this.guessbutton.UseVisualStyleBackColor = true;
+            this.guessbutton.Click += new System.EventHandler(this.guessbutton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(219, 249);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(342, 43);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Uzak";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(586, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Yakın";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.guessbutton);
+            this.Controls.Add(this.guess);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox guess;
+        private System.Windows.Forms.Button guessbutton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BA_Boost_SayiTahmin
+{
+    public partial class Form1 : Form
+    {
+        
+        int rnd = new Random().Next(1, 100);
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void guessbutton_Click(object sender, EventArgs e)
+        {
+            decimal proximity=0;
+            decimal input = Convert.ToDecimal(guess.Text.Trim());
+            
+            if (input < rnd) {
+                proximity = (input / Convert.ToDecimal(rnd)) * 100;
+            }
+            else if (input > rnd) {
+
+                proximity = (Convert.ToDecimal(rnd) / input) * 100;
+            }
+            else if (input == Convert.ToDecimal(rnd)) {
+                proximity = 100;
+                MessageBox.Show($"Tebrikler seçilen sayı {rnd} idi.");
+                proximity = 0;
+                progressBar1.Value = 0;
+                rnd = new Random().Next(1, 100);
+                guess.Clear();
+                return;
+            }
+            
+            progressBar1.Value = Convert.ToInt32(proximity);
+            guess.Clear();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Maximum=100;
+            progressBar1.Minimum = 0;
+            
+            
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
